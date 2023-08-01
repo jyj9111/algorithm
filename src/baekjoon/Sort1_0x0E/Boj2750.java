7269b12107bad3508b8eae191af2963f775d414f
jyj9111 <mrjang9111@gmail.com>
2023-08-01 14:38:20 +0900

    feat: 바킹독 강의 0x0E 정렬1 - 백준문제 제출코드 추가

---
+++ src/baekjoon/Sort1_0x0E/Boj2750.java
@@ -0,0 +1,24 @@
+package baekjoon.Sort1_0x0E;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Boj2750 { // Boj2750: 수 정렬하기
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int nNum =  Integer.parseInt(reader.readLine());
+
+        int[] arr =  new int[nNum];
+        for (int i = 0; i < nNum; i++) {
+            arr[i] = Integer.parseInt(reader.readLine());
+        }
+        Arrays.sort(arr);
+        StringBuilder answer = new StringBuilder();
+        for (int i : arr) {
+            answer.append(i).append('\n');
+        }
+        System.out.println(answer);
+    }
+}
---
+++ src/baekjoon/Sort1_0x0E/Boj2751.java
@@ -0,0 +1,56 @@
+package baekjoon.Sort1_0x0E;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj2751 { // Boj2751: 수 정렬하기 2
+    private int[] arr;
+    private int[] temp;
+
+    private void merge(int st, int ed) {
+        int mid = (st + ed) / 2;
+        int leftIdx = st;
+        int rightIdx = mid;
+
+        for (int i = st; i < ed; i++) {
+            if (rightIdx == ed) temp[i] = arr[leftIdx++];
+            else if (leftIdx == mid) temp[i] = arr[rightIdx++];
+            else if (arr[leftIdx] <= arr[rightIdx]) temp[i] = arr[leftIdx++];
+            else temp[i] = arr[rightIdx++];
+        }
+
+        for (int i = st; i < ed; i++) {
+            arr[i] = temp[i];
+        }
+    }
+
+    public void mergeSort(int st, int ed) {
+        if (ed == st + 1) return;
+        int mid = (st + ed) / 2;
+        mergeSort(st, mid);
+        mergeSort(mid, ed);
+        merge(st, ed);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int nNum =  Integer.parseInt(reader.readLine());
+
+        Boj2751 obj = new Boj2751();
+        obj.arr =  new int[nNum];
+        obj.temp =  new int[nNum];
+
+        for (int i = 0; i < nNum; i++) {
+            obj.arr[i] = Integer.parseInt(reader.readLine());
+        }
+
+        obj.mergeSort(0, nNum);
+
+        StringBuilder answer = new StringBuilder();
+        for (int i : obj.arr) {
+            answer.append(i).append('\n');
+        }
+        System.out.println(answer);
+    }
+}
